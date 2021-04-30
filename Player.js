5df20db1fafcb418eb821c7a19830b041df5ddca
rzientara <46692302+rzientara@users.noreply.github.com>
2021-04-29 22:18:27 -0500

    Adds tournament and player classes.

---
+++ Player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(playerName, leagueNumber, dateJoined, birthday, hometown, rankChips) {
+    this.playerName = playerName;
+    this.leagueNumber = leagueNumber;
+    this.dateJoined = dateJoined;
+    this.birthday = birthday;
+    this.hometown = hometown;
+    this.rankChips = rankChips;
+  }
+}
---
+++ Tournament.js
@@ -0,0 +1,14 @@
+class Tournament {
+  constructor(tournamentName, location, maxPlayers, tournamentDate, tournamentID, winningHand, firstPlace, secondPlace, thirdPlace, fourthPlace) {
+  this.tournamentName = tournamentName;
+  this.location = location;
+  this.maxPlayers = maxPlayers;
+  this.tournamentDate = tournamentDate;
+  this.tournamentID = tournamentID;
+  this.winningHand = winningHand;
+  this.firstPlace = firstPlace;
+  this.secondPlace = secondPlace;
+  this.thirdPlace = thirdPlace;
+  this.fourthPlace = fourthPlace;
+  }
+}
